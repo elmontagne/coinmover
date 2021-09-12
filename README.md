500231789f0fb6ff46d7a4a98691458095d29fcb
elmontagne <90505490+elmontagne@users.noreply.github.com>
2021-09-12 16:48:26 +0200

    final first version

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # coinmover
+FTX:
+login, make sure main is selected. 
+Create subaccount (example: savings)
+in your api keys: enable transfers
+
+edit config.ini , fill in api key, secret 
+source_sub = subaccount which needs where you want to check for profit
+dest_sub = subaccount where you want to move the profit to
+sleeptime = after how many minutes you want to run this again
+discord_webhook = your webhook if you want to use discord notifications
+
+Not done yet:
+* maximum margin in use 
+* check if all files are in place
--- wallet.py
+++ wallet.py
@@ -2,11 +2,17 @@
 # coding: utf-8
 
 # In[ ]:
+# Created by elmontagne
+### author:  elmontagne
+### discord: Daisy#2718
+### site:    https://github.com/elmontagne/coinmover
+### Changelog:
+### * 
 
 
 import requests
 import time, json
-from time import sleep
+import time
 import FTX_Class
 import configparser
 import os.path
@@ -20,41 +26,60 @@ sleeptime = config['coinmover']['sleeptime']
 maxmargin = config['coinmover']['maxmargin']
 apikey = config['coinmover']['apikey']
 apisecret = config['coinmover']['apisecret']
+percentage_move = config['coinmover']['percentage_move']
+discord_webhook = config['coinmover']['discord_webhook']
+
+currenttime = time.localtime()
+timenow = time.strftime("%I:%M:%S %p", currenttime)
+sleeptime = int(sleeptime)*60
+
+
 
 c = FTX_Class.FtxClient(api_key=apikey, api_secret=apisecret,subaccount_name=source_sub)
 
-file_exists = os.path.isfile('status')
-if file_exists:
-	with open('status', 'r') as ff:
-		old_balance=ff.readline()
-else:
-	f = open("status","w")
-	f.write("0")	
-	old_balance=0
+while True:
+	print(timenow," Checking...")
+	file_exists = os.path.isfile('status')
+	if file_exists:
+		with open('status', 'r') as ff:
+			old_balance=ff.readline()
+	else:
+		f = open("status","w")
+		f.write("0")	
+		old_balance=0
 	
-print("Old balance: ",old_balance)	
+	print("Old balance: ",old_balance)	
 
-check = c.get_wallet()
-print("Balance of degen :")
-print(check[0]['coin'], ' ', check[0]['total'])
+	check = c.get_wallet()
+	print("Balance of degen :")
+	print(check[0]['coin'], ' ', check[0]['total'])
 
-if float(old_balance) != 0 and check[0]['total'] > float(old_balance):
-	print("we made profit!")
-	print(check[0]['total'] - float(old_balance))
-	transfer = check[0]['total'] - float(old_balance)
-	print("transferring: ",transfer)
-	move_funds = c.move_funds('USD', float(transfer), dest_sub)   
-else:
-	print("No profit this time")
-	print(check[0]['total'] - float(old_balance))
-	
-for x in check:
-	print(x['coin'], ' ', x['total'], ' ', x['free'])
-	
-	
-with open('status', 'w') as f:
-    f.write(str(check[0]['total']))
+	if float(old_balance) != 0 and check[0]['total'] > float(old_balance):
+		print("we made profit!")
+		print(check[0]['total'] - float(old_balance))
+		profit = check[0]['total'] - float(old_balance)
+		transfer = float(profit) * float(percentage_move) / 100
+		print("transferring: ",check_coin,transfer," to: ", dest_sub)
+		#move_funds = c.move_funds(check_coin, float(transfer), dest_sub)  
+		status_message = "Transferred: " + check_coin + " " + str(transfer) + " to: " + dest_sub
+		data = {
+    "content" : status_message
+}
+		if discord_webhook != '':
+			result = requests.post(discord_webhook, json = data)
 
+	else:
+		print("No profit this time")
+		print(check[0]['total'] - float(old_balance))
+	
+	for x in check:
+		print(x['coin'], ' ', x['total'], ' ', x['free'])
+	
+	
+	with open('status', 'w') as f:
+		f.write(str(check[0]['total']))
+	print("Sleeping for ",sleeptime,"seconds")
+	time.sleep(sleeptime)
  
 #all_wallet = c.get_allwallet()
 #print(all_wallet['main'][1]['coin'])
